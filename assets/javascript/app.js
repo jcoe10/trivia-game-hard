5f2d2c649126d21cd8dbe576ad16a0a2c65479a3
jcoe10 <jacob.e.coe@gmail.com>
2017-08-31 09:54:39 -0400

    added index and assets

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    var currentQuestion = 0;
+    var $question = $('.question');
+    var totalQuestions = $('.question').length;
+    var correct = 0;
+    var wrong = 0;
+    var score = 0;
+    $question.hide();
+
+    $(".start").on( "click", function(event) {
+        console.log(event.key);
+
+    
+    });
+
+});
